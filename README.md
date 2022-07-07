5af71509e353c54a462c6091f76e3b3319dc2bcb
Legoshin-Alexandr <aleksandr.legoshin@ugasoft.com>
2022-07-07 09:36:15 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+##Repository for practice with different testing tools 
---
+++ src/test/java/qa/auto/Test.java
@@ -0,0 +1,14 @@
+package qa.auto;
+
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        List<String> wordsList = List.of("Create", "list", "kind", "of", "a", "literal", "is", "a", "great", "idea");
+        String resultString = String.join(" ", wordsList);
+        System.out.println(resultString);
+    }
+
+}
